c8c522563ab502f17ddcc69f76d15443f26b995a
Jiekai Sun <jiekai.sun@studio.unibo.it>
2026-04-21 20:49:43 +0200

    chore: basic pacman-game setup

---
+++ pacman-game/build.gradle.kts
@@ -0,0 +1,31 @@
+plugins {
+    java
+    application
+}
+
+group = "it.unibo"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:6.0.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(25)
+    }
+}
+
+application {
+    mainClass.set("it.unibo.Main")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
