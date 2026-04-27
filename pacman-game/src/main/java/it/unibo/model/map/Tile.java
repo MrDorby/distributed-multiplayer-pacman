8776789be7cdb4c1c87a0b4c68432f09178a132e
Simone Carpi <simone.carpi@studio.unibo.it>
2026-04-27 18:26:54 +0200

    First implementation of the Map and Tile interfaces

--- pacman-game/src/main/java/it/unibo/model/map/Tile.java
+++ pacman-game/src/main/java/it/unibo/model/map/Tile.java
@@ -5,9 +5,36 @@ import it.unibo.model.entities.Dot;
 
 import java.util.Optional;
 
+/**
+ * Models the concept of a cell inside the map.
+ */
 public interface Tile {
+
+    /**
+     * @return a boolean true if the Tile is a Wall or false otherwise.
+     */
     boolean isWall();
+
+    /**
+     * This method let you get the dot ì, if present, inside the tile.
+     * @return an Optional of Dot.
+     */
     Optional<Dot> getDot();
+
+    /**
+     * Gives the position of the center of the Tile.
+     * @return a Vector2D corresponding to the coordinates of the center.
+     */
     Vector2D getCenterPosition();
+
+    /**
+     * Gives the position of the Tile inside the matrix (Map).
+     * @return a Vector2D corresponding to the coordinates of the Tile.
+     */
     Vector2D getMatrixPosition();
+
+    /**
+     * @return the TileType of the corresponding Tile.
+     */
+    TileType getTileType();
 }
