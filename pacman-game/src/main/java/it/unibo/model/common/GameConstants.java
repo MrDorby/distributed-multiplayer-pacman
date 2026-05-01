593cd74df0d3b844838f2febd79f43c65b2910f2
Jiekai Sun <jiekai.sun@studio.unibo.it>
2026-05-01 16:18:12 +0200

    feat: add dummy gamecontext factory and constants class

---
+++ pacman-game/src/main/java/it/unibo/model/common/GameConstants.java
@@ -0,0 +1,9 @@
+package it.unibo.model.common;
+
+public class GameConstants {
+    // Prevent instantiation
+    private GameConstants() {}
+
+    public static final int TILE_SIZE = 32;
+    public static final int GAME_DURATION_SECONDS = 180;
+}
