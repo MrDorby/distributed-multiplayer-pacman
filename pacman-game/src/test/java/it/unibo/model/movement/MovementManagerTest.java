4c92be77d0451dfb7fdedce02472f4c063d71730
Alessandro Ronchi <alessandro.ronchi7@studio.unibo.it>
2026-05-31 19:47:31 +0200

    Refactored and wrote some tests in MovementManagerTest

--- pacman-game/src/test/java/it/unibo/model/movement/MovementManagerTest.java
+++ pacman-game/src/test/java/it/unibo/model/movement/MovementManagerTest.java
@@ -5,24 +5,29 @@ import it.unibo.model.common.MatrixCoordinates;
 import it.unibo.model.common.Vector2D;
 import it.unibo.model.map.FourPlayersGameMapFactory;
 import it.unibo.model.map.GameMap;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 import org.junit.jupiter.params.provider.CsvSource;
 
+import java.util.stream.IntStream;
+
 import static it.unibo.model.common.GameConstants.GameEntityFeatures.PACMAN;
+import static it.unibo.model.common.GameConstants.TILE_SIZE;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class MovementManagerTest {
-    private final MatrixCoordinates initialCoordinates = new MatrixCoordinates(3, 3);
-    private final GameMap map = new FourPlayersGameMapFactory().fromJSON("it/unibo/maps/movement_test_map.json");
-    private final Vector2D initialPosition = map.getTile(initialCoordinates).getCenterPosition(); // TODO: generalize the initial position
     private static final int MOVEMENT_VELOCITY = PACMAN.getVelocity();
+    private static final MatrixCoordinates INITIAL_MOVEMENT_POSITION = new MatrixCoordinates(3, 3);
+    private static final String ROOT = "it/unibo/";
+    private static final String MAPS = ROOT + "maps/";
+    private static final String MOVEMENT = "/" + ROOT + "movement/";
 
+    private final GameMap map = new FourPlayersGameMapFactory().fromJSON(MAPS + "movement_test_map.json");
     private MovementManager movement;
 
-    @BeforeEach
-    void init() {
-        this.movement = new MovementManagerImpl(map, initialCoordinates, MOVEMENT_VELOCITY);
+    private Vector2D initializeMovement(final MatrixCoordinates coordinates) {
+        this.movement = new MovementManagerImpl(map, coordinates, MOVEMENT_VELOCITY);
+        return map.getTile(coordinates).getCenterPosition();
     }
 
     private Vector2D getExpectedFinalPosition(
@@ -39,23 +44,39 @@ public class MovementManagerTest {
     }
 
     @ParameterizedTest
-    @CsvSource({
-            "UP",
-            "DOWN",
-            "LEFT",
-            "RIGHT"
-    })
+    @CsvFileSource(resources = MOVEMENT + "all_directions.csv")
     void testSpecificDirectionMovement(Direction movementDirection) {
+        Vector2D initialPosition = initializeMovement(INITIAL_MOVEMENT_POSITION);
         movement.changeDirection(movementDirection);
         assertEquals(getExpectedFinalPosition(initialPosition, movementDirection, 1), movement.move());
     }
 
-    void testCannotMove() {
-        // TODO: test that the movement is not allowed towards walls
+    @ParameterizedTest
+    @CsvFileSource(resources = MOVEMENT + "all_directions.csv")
+    void testCannotMove(Direction movementDirection) {
+        Vector2D initialPosition = initializeMovement(new MatrixCoordinates(5, 7));
+        movement.changeDirection(movementDirection);
+        assertEquals(initialPosition, movement.move());
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(resources = MOVEMENT + "all_directions.csv")
+    void testMultipleTilesMovement(Direction movementDirection) {
+        Vector2D initialPosition = initializeMovement(INITIAL_MOVEMENT_POSITION);
+        movement.changeDirection(movementDirection);
+        int numberOfMovements = (2 * TILE_SIZE) / MOVEMENT_VELOCITY;
+        IntStream.range(0, numberOfMovements - 1).forEach((e) -> movement.move());
+        assertEquals(getExpectedFinalPosition(initialPosition, movementDirection, numberOfMovements), movement.move());
     }
 
     void testInvertDirection() {
         // TODO: test that the direction of movement can be inverted while moving
+    }
+
+    void testInvertDirectionAgainstWall() {
+        // TODO: test that the direction of movement can be inverted even if the tile behind the starting point is a
+        //  wall. Once we are back to the initial position, test that the movement does not move further, because of
+        //  the wall.
     }
 
     void testTurn() {
